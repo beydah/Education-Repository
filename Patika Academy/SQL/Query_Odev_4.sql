bb35819a72ecb29129e65848a1f3d6a2dccaa076
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-09-11 12:44:27 +0300

    Add files via upload

---
+++ Patika Academy/SQL/Query_Odev_4.sql
@@ -0,0 +1,14 @@
+/*1.SORU*/
+--SELECT DISTINCT replacement_cost FROM film;
+
+/*2. SORU*/
+--SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+/*3. SORU*/
+-- SELECT COUNT(*) FROM film WHERE title ~~ 'T%' AND rating = 'G';
+
+/*4. SORU*/
+--SELECT COUNT(country) FROM country WHERE country ~~ '_____';
+
+/*5. SORU*/
+SELECT COUNT(city) FROM city WHERE city ~~* '%r';
